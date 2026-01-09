395a057d16d13d86b123042d5da3f2957c321424
RY4N <ryanwtf88@gmail.com>
2026-01-09 15:05:20 +0000

    fix: correctly configure bun runtime and clean up repository

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,3 @@
-import { handle } from '@hono/node-server/vercel'
 import { app } from '../src/index'
 
-export const config = {
-    runtime: 'nodejs'
-}
-
-export default handle(app)
+export default app
